7df113a89eb11c82cd521ee41fdcaebe402f79e7
DesiFrontend <157307500+DesiFrontend@users.noreply.github.com>
2024-04-23 13:43:10 -0600

    Update RegisterPage.jsx

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -83,11 +83,9 @@ function RegisterPage() {
                 }
             })
             .catch((error) => {
-                setTimeout(() => {
                 setMessage('Error al registrarse');
                 console.log('Error en la petición:' + error);
-            }, 2000);
-        }
+            });
     }
 
     return (
