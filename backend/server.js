69fcbff069988a6d8231358f3b91f9fcf23fa2d6
DesiFrontend <157307500+DesiFrontend@users.noreply.github.com>
2024-04-24 13:09:58 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,7 @@ const dbConnection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    port: 3307,
 });
 
 try {
